5db3cd8a92b3a999c7e79d5357c68a9386cb0bb1
Wildan Hanani <wildanhanani@gmail.com>
2021-06-06 15:09:17 +0700

    read_json

--- read.js
+++ read.js
@@ -1,12 +1,47 @@
 const fs = require('fs');
 
-// Read users.json file
 fs.readFile('data.json', function (err, data) {
-  // Check for errors
   if (err) throw err;
 
-  // Converting to JSON
-  const users = JSON.parse(data);
+  const user = JSON.parse(data);
 
-  console.log(users.placement); // Print users
+  let electronics = user.filter((user) => user.type == 'electronic');
+  console.log('electronic:', electronics);
+
+  let furniture = user.filter((user) => user.type === 'furniture');
+  console.log('furniture:', furniture);
+
+  let colour = user.filter((user) => user.tags === 'brown');
+  console.log('colour:', furniture);
+
+  let meeting = user.filter((user) => user.placement.name === 'Meeting Room');
+  console.log('Room:', meeting);
+
+  let date = user.filter((user) => user.purchased_at === 1579190642);
+  console.log('Date:', date);
+
+  // karena tanggal 16 januari 2020 tidak saya temukan diantara timestamp di file json maka saya mengambil timestamp secara acak
 });
+
+// var timestamp = 1579190471;
+// var date = new Date(timestamp);
+// console.log(date.getTime());
+// console.log(date);
+
+// var timestamp = 1579190471;
+// var date = new Date(timestamp);
+
+// console.log(
+//   'Date: ' +
+//     date.getDate() +
+//     '/' +
+//     (date.getMonth() + 1) +
+//     '/' +
+//     date.getFullYear() +
+//     ' ' +
+//     date.getHours() +
+//     ':' +
+//     date.getMinutes() +
+//     ':' +
+//     date.getSeconds()
+// );
