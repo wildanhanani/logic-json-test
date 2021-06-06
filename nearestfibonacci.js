3385f37cf3981613ce7cdfcc225c3badb353fbef
Wildan Hanani <wildanhanani@gmail.com>
2021-06-07 01:04:46 +0700

    fibonacci

--- nearestfibonacci.js
+++ nearestfibonacci.js
@@ -1,9 +1,31 @@
-function nextFibonacci(n) {
-  let a = (n * (1 + Math.sqrt(5))) / 2;
-  return Math.round(a);
+function fibonacci(n) {
+  return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-// Driver code
+function findNearestFibNumber(arr) {
+  sum = 0;
+  arr.map((value) => {
+    sum += value;
+  });
 
-let n = 19;
-console.log(nextFibonacci(n));
+  let i = 0;
+  let cond = true;
+  let nearestFib = 0;
+
+  while (cond) {
+    let fib = fibonacci(i);
+    if (fib >= sum) {
+      cond = false;
+      nearestFib = fib;
+    }
+    i++;
+  }
+
+  console.log(
+    `Total array ${sum}, and nearest fibonacci is ${nearestFib}, so result is: ${
+      nearestFib - sum
+    }`
+  );
+}
+
+findNearestFibNumber([15, 1, 3]);
